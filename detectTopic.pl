97cb9a94a9e8ee188c969713c847b0647ae6e1bf
joey0214 <joey0576@163.com>
2013-04-05 15:46:18 +0800

    Create detectTopic.pl

---
+++ detectTopic.pl
@@ -0,0 +1,32 @@
+#!/local/bin/perl -w
+#@author: joey
+#@version: 2013-04-05
+#@bug report to: joey0576@163.com
+
+use strict;
+use warnings;
+
+if (@ARGV != 2){
+  print "Usage: perl $0 infile outfile \n";
+	exit;
+}
+my ($infile, $outfile) =@ARGV; 
+open(OUTFILE, ">$outfile");
+open (FILE, "<$infile");
+my @fileContent =<FILE>;
+my $wholeString ="";
+foreach my $_(@fileContent) {
+	chop($_);
+	$wholeString=$wholeString.$_;
+	
+}
+my @words=split(/\b/,$wholeString);
+my (%count, $word);
+foreach $word(@words){
+	$count{$word} +=1;
+}
+foreach $word(sort keys %count){
+	print OUTFILE "$word \t $count{$word}\n";
+}
+close FILE;
+close OUTFILE;
